90e4c179ffc8c5bf787747f7ce1680b4db7fdedc
MAqilSadik <aqilsadik@users.noreply.github.com>
2020-11-05 23:44:06 +0700

    Create Praktikum2No8.py

---
+++ Praktikum2No8.py
@@ -0,0 +1,20 @@
+def luasSegitiga(a,t):
+    luas = a * t / 2
+    return luas
+
+alasSegitiga1 = 10
+alasSegitiga2 = 15
+tinggiSegitiga1 = 20
+tinggiSegitiga2 = 45
+
+print('luas segitiga dengan alas',alasSegitiga1,
+      'dan tinggi',tinggiSegitiga1,'adalah',
+      luasSegitiga(alasSegitiga1,tinggiSegitiga1))
+
+print('luas segitiga dengan alas',alasSegitiga2,
+      'dan tinggi',tinggiSegitiga2,'adalah',
+      luasSegitiga(alasSegitiga2,tinggiSegitiga2))
+
+
+print('Total Luas Segitiga : ',luasSegitiga(10,20)+luasSegitiga(15,45))
+
