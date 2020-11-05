44d1072118794c5d30dbcdf1c3ce1cdc3a58c250
MAqilSadik <aqilsadik@users.noreply.github.com>
2020-11-05 22:58:43 +0700

    Create Praktikum2No7.py

---
+++ Praktikum2No7.py
@@ -0,0 +1,13 @@
+def luasSegitiga(a,t):
+    luas = a * t / 2
+    print('luas segitiga dg alas',a,
+          'dan tinggi',t,'adalah',luas)
+
+alas1 = 10
+tinggi1 = 20
+
+alas2 = 15
+tinggi2 = 45
+
+luasSegitiga(alas1,tinggi1)
+luasSegitiga(alas2,tinggi2)
